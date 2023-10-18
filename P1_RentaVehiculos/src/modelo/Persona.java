192aee4b5cad8543bd58b4d2c8c42678ac44d158
Dav1d10 <d.carol@uniandes.edu.co>
2023-10-17 23:57:38 -0500

    cambios

---
+++ P1_RentaVehiculos/src/modelo/Persona.java
@@ -0,0 +1,15 @@
+package modelo;
+
+import java.io.File;
+
+public class Persona {
+	
+	private String nombre;
+	private String telefono;
+	private String fechadeNacimiento;
+	private String nacionalidad;
+	private File documentodeIdentidad;
+
+}
+
+
